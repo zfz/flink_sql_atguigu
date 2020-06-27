87618cbde5b2e0adf02d943c22102d46af602ec5
zfz <zhoufangzhang@gmail.com>
2020-06-27 17:42:41 -0400

    Add table UDF

--- src/main/scala/com/atguigu/table/ch06_udf/TableFunctionExample.scala
+++ src/main/scala/com/atguigu/table/ch06_udf/TableFunctionExample.scala
@@ -7,10 +7,10 @@ import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.table.api.Table
 import org.apache.flink.table.api.scala._
-import org.apache.flink.table.functions.ScalarFunction
+import org.apache.flink.table.functions.TableFunction
 import org.apache.flink.types.Row
 
-object ScalarFunctionExample {
+object TableFunctionExample {
   def main(args: Array[String]): Unit = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     env.setParallelism(1)
@@ -32,40 +32,46 @@ object ScalarFunctionExample {
       .assignTimestampsAndWatermarks(
         new BoundedOutOfOrdernessTimestampExtractor[SensorReading](Time.seconds(1)) {
           override def extractTimestamp(element: SensorReading): Long = element.timestamp * 1000L
-      } )
+        } )
 
     // 3. 将流转换成表，直接定义时间字段
     val sensorTable: Table = tableEnv.fromDataStream(dataStream, 'id, 'temperature, 'timestamp.rowtime as 'ts)
 
-    // 4. 使用自定义的Hash函数，求ID的Hash值
-    val hashCode = new HashCode(1.23)
+    // 4. 先创建一个UDF对象
+    val split = new Split("_")
 
-    // 5.1 Table API 调用方式
+    // 5.1 Table API调用
     val resultTable: Table = sensorTable
-      .select('id, 'ts, hashCode('id))
-    resultTable.toAppendStream[Row].print("table")
+      // 侧向连接，应用TableFunction，也可以使用leftOuterJoinLateral
+      .joinLateral(split('id) as ('word, 'length))
+      .select('id, 'ts, 'word, 'length)
 
-    // 5.2 SQL调用方式，首先要注册表和函数
+    // 5.2 SQL调用
     tableEnv.createTemporaryView("sensor", sensorTable)
-    tableEnv.registerFunction("hashCode", hashCode)
-    val resultSQLTable: Table = tableEnv.sqlQuery(
+    tableEnv.registerFunction("split", split)
+    val resultSQLTable = tableEnv.sqlQuery(
       """
-        |select id, ts, hashCode(id)
-        |from sensor
+        | select id, ts, word, length
+        | from sensor, lateral table(split(id)) as splitid(word, length)
       """.stripMargin)
-    resultSQLTable.toAppendStream[Row].print("sql")
 
-    env.execute("Scalar UDF")
+    resultTable.toAppendStream[Row].print("result")
+    resultSQLTable.toAppendStream[Row].print("sql result")
+
+    env.execute("Table Function Example")
   }
 }
 
 /**
- * 自定义求HashCode的标量函数
+ * 自定义TableFunction，实现分割字符串并统计长度(word, length)
+ * 一行数据拆出多行
  */
-class HashCode(factor: Double) extends ScalarFunction {
-  // 必须实现eval方法，但是也没写在ScalarFunction的抽象类里
-  // 返回值不能为Unit
-  def eval( value: String ): Int = {
-    (value.hashCode * factor).toInt
+class Split(separator: String) extends TableFunction[(String, Int)] {
+  // 返回值不能为Unit，使用collector收集数据
+  def eval(value: String): Unit = {
+    value.split(separator).foreach(
+      word => collect(word, word.length)
+    )
+
   }
 }
