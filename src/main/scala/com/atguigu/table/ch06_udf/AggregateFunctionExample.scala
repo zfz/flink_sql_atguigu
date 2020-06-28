05091645b1ff7808a3440f50c5010258b3f4dc90
zfz <zhoufangzhang@gmail.com>
2020-06-28 14:15:38 -0400

    Add table UDAF

--- src/main/scala/com/atguigu/table/ch06_udf/AggregateFunctionExample.scala
+++ src/main/scala/com/atguigu/table/ch06_udf/AggregateFunctionExample.scala
@@ -7,10 +7,10 @@ import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.table.api.Table
 import org.apache.flink.table.api.scala._
-import org.apache.flink.table.functions.TableFunction
+import org.apache.flink.table.functions.AggregateFunction
 import org.apache.flink.types.Row
 
-object TableFunctionExample {
+object AggregateFunctionExample {
   def main(args: Array[String]): Unit = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     env.setParallelism(1)
@@ -37,40 +37,43 @@ object TableFunctionExample {
     // 3. 将流转换成表，直接定义时间字段
     val sensorTable: Table = tableEnv.fromDataStream(dataStream, 'id, 'temperature, 'timestamp.rowtime as 'ts)
 
-    // 4. 先创建一个UDF对象
-    val split = new Split("_")
+    // 4. 创建一个聚合函数的实例
+    val avgTemperature = new AvgTemperature()
 
-    // 5.1 Table API调用
-    val resultTable: Table = sensorTable
-      // 侧向连接，应用TableFunction，也可以使用leftOuterJoinLateral
-      .joinLateral(split('id) as ('word, 'length))
-      .select('id, 'ts, 'word, 'length)
+    // 5. Table API调用
+    val resultTable = sensorTable
+      .groupBy('id)
+      .aggregate(avgTemperature('temperature) as 'avg_temperature )
+      .select('id, 'avg_temperature)
 
-    // 5.2 SQL调用
-    tableEnv.createTemporaryView("sensor", sensorTable)
-    tableEnv.registerFunction("split", split)
-    val resultSQLTable = tableEnv.sqlQuery(
-      """
-        | select id, ts, word, length
-        | from sensor, lateral table(split(id)) as splitid(word, length)
-      """.stripMargin)
+    resultTable.toRetractStream[Row].print("avg result")
 
-    resultTable.toAppendStream[Row].print("result")
-    resultSQLTable.toAppendStream[Row].print("sql result")
+    env.execute("UDAF")
+  }
+}
 
-    env.execute("Table Function Example")
+
+/**
+ * 自定义聚合函数
+ * T 返回类型
+ * ACC 聚合类型
+ * 输入类型在eval中指定
+ **/
+class AvgTemperature extends AggregateFunction[Double, AvgTemperatureACC] {
+  override def getValue(acc: AvgTemperatureACC): Double = acc.sum / acc.cnt
+
+  override def createAccumulator(): AvgTemperatureACC = new AvgTemperatureACC()
+
+  def accumulate(acc: AvgTemperatureACC, temperature: Double): Unit = {
+    acc.sum += temperature
+    acc.cnt += 1
   }
 }
 
 /**
- * 自定义TableFunction，实现分割字符串并统计长度(word, length)
- * 一行数据拆出多行
- */
-class Split(separator: String) extends TableFunction[(String, Int)] {
-  // 返回值不能为Unit，使用collector收集数据
-  def eval(value: String): Unit = {
-    value.split(separator).foreach(
-      word => collect(word, word.length)
-    )
-  }
+ * 专门定义一个聚合函数的状态类，用于保存聚合状态(sum，count)
+ **/
+class AvgTemperatureACC {
+  var sum: Double = 0.0
+  var cnt: Int = 0
 }
